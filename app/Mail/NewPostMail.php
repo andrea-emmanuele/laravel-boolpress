56654bbf8f3e65acf74b7b143f4d2e5b65095f33
andrea-emmanuele <contact@andreaemmanuele.it>
2021-05-31 17:33:34 +0200

    Mail sent when new post is created

---
+++ app/Mail/NewPostMail.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class NewPostMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    protected $message = "Thank you for being an active user!";
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('mails.newPost');
+    }
+}
